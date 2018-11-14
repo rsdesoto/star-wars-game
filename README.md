8e13e19aa924b63a90ca8b4f8978882a6451dd7a
Ry <ry.e.desoto@gmail.com>
2018-11-14 00:08:44 -0600

    finished win/lose results, added pictures of heroes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # star-wars-game
+
 taking a stab at the harder homework
+
+### Credit
+
+All images poached from wookieepedia. I know the Bastila and Exile photos are lo-fi; the games came out in 2003 and 2004, respectively, and most of the art of them (especially Meetra Surik, or whatever her "canon" name is) are either not great quality or they don't have cool laser swords.
+
+### "Hey Ry, you've gone with all women, except for Darth Revan, what's with that?"
+
+1. I think it's a mistake, honestly, to give a canonical gender/appearance/etc to any player character for any player characters where you can pick their traits. Fans get attached to "their" Revan (or Exile, or Cmdr Shepard, or Courier Six, or etc) and then have to deal with some other, incorrect version in supplemental canon. (Supplemental canon isn't real anyway, and the EU, rest its ridiculous soul, is no longer with us)
+1. Darth Revan is a woman, fight me
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,15 @@
-// 0. take all 4 fighters and assign them to the choose your fighter screen. prompt the
-// user to choose their fighter
+/////////////////////////////////////////////////////////////////
+/////////////////////////// VARIABLES ///////////////////////////
+/////////////////////////////////////////////////////////////////
 
+// heroes array for generating interactive buttons
+heroesArray = ["bastila", "revan", "kreia", "exile"];
+
+// hero stat objects
 var kreiaobj = {
     name: "kreia",
-    health: 50,
-    attack: 10,
+    health: 500,
+    attack: 20,
     counter: 20
 };
 
@@ -29,6 +34,23 @@ var exileobj = {
     counter: 30
 };
 
+function setObj() {
+    // hero stat objects
+    kreiaobj.health = 500;
+    kreiaobj.attack = 20;
+
+    revanobj.health = 25;
+    revanobj.attack = 20;
+
+    bastilaobj.health = 75;
+    bastilaobj.attack = 5;
+
+    exileobj.health = 90;
+    exileobj.attack = 5;
+}
+
+// associative array (for getting from text strings to objects -- i.e., if i have the string
+// "bastila", i want to be able to translate that to the OBJECT bastila)
 var heroObj = {
     bastila: bastilaobj,
     kreia: kreiaobj,
@@ -36,103 +58,200 @@ var heroObj = {
     exile: exileobj
 };
 
-function temphide() {
-    $("#attackbutton").hide();
-}
-
-temphide();
-
 // generate each character's stats
 
 // 1. on first click, move the fighter up to the fighter section
 // 2. on second click, move the opponent down to the opponent section
 // on second click, also unhide the attack button
 
-var heroChosen = false;
-var opponentChosen = false;
+// game control stats -- boolean values for if the hero and opponent have been chosen yet
+var heroChosen;
+var opponentChosen;
 
+// game control stats -- hero and opponent chosen (will change each round)
 var fighter;
 var opponent;
 
-$(".hero").on("click", function(event) {
-    if (!heroChosen) {
-        fighter = $(event.target).attr("id");
-        console.log(fighter);
-        var temp = $("#" + fighter);
-        console.log(temp.attr("class"));
-        temp.appendTo($("#my-hero"));
-        heroChosen = true;
-    } else if (!opponentChosen) {
-        opponent = $(event.target).attr("id");
-        console.log(opponent);
-        var temp = $("#" + opponent);
-        console.log(temp.attr("class"));
-        temp.appendTo($("#enemy"));
-        opponentChosen = true;
+// game control stats -- how many enemies are left before end of game
+var remainingEnemy;
 
-        $("#attackbutton").show();
-    }
-});
+// game control stats -- how many rounds you have fought. This is what the attack score each round is multiplied by.
+var roundcount;
 
-// 3. on attack button click, do the attack/defense. check for game end condition.
-
-// 4. on game end, reset everything
-
-// on every click of attack, attack = attack + attack(base)
-// on every click of attack, take dmg equal to opponent's counter
-
-// on button click - roundcount goes up by one and
-// is input into attack
-
-// attack(kreia, revan, 3);
-
-roundcount = 0;
-$("#attackbutton").on("click", function() {
-    roundcount++;
-    console.log(roundcount);
-    console.log(fighter);
-    console.log(opponent);
-    attack(heroObj[fighter], heroObj[opponent], roundcount);
-});
+/////////////////////////////////////////////////////////////////
+/////////////////////////// FUNCTIONS ///////////////////////////
+/////////////////////////////////////////////////////////////////
 
 function attack(fighter, opponent, roundcount) {
+    // for the fighter - use attack power and multiply by how many times they have attacked
     fighterAttack = fighter.attack * roundcount;
     fighterHealth = fighter.health;
-
-    alert(fighter.name);
-    alert(opponent.name);
 
     // for the opponent - only use the counter attack, and do not multiply by what round it is
     opponentAttack = opponent.counter;
     opponentHealth = opponent.health;
 
+    // calculate the updated health for both fighter and opponent and update the objects
     fighter.health = fighterHealth - opponentAttack;
     opponent.health = opponentHealth - fighterAttack;
 
-    console.log(
+    // create updates on stats
+    $("#actionUpdate1").html(
         fighter.name +
             " has hit " +
             opponent.name +
             " for " +
             fighterAttack +
-            " points of damage!"
+            " points of damage! " +
+            opponent.name +
+            " has " +
+            opponent.health +
+            " hit points left!"
     );
-    console.log(
+    $("#actionUpdate2").html(
         opponent.name +
             " has hit " +
             fighter.name +
             " for " +
             opponentAttack +
-            " points of damage!"
+            " points of damage! " +
+            fighter.name +
+            " has " +
+            fighter.health +
+            " hit points left!"
     );
 
-    console.log(fighter.name + " has " + fighter.health + " hit points left!");
-    console.log(
-        opponent.name + " has " + opponent.health + " hit points left!"
-    );
+    // check to see if a fighter is out
+    checkEndCondition(fighter.health, opponent.health);
 }
 
-// // attack(heroObj[fighter], heroObj[opponent], 2);
-// //console.log(bastila.name);
+// make a function to check for game end -- if either party's health is below 0, round (and possibly game) end
+function checkEndCondition(fighterHealth, opponentHealth) {
+    if (fighterHealth < 0 && opponentHealth < 0) {
+        $("#matchup").html("<h2>You have defeated each other! Game over</h2>");
+        $("#attackbutton").hide();
+        $("#resetbutton").show();
+        $("#loserDisplay").show();
+        $("#my-hero").empty();
+        $("#enemy").empty();
+    } else if (fighterHealth < 0) {
+        $("#matchup").html("<h2>You lose :c</h2>");
+        $("#attackbutton").hide();
+        $("#resetbutton").show();
+        $("#loserDisplay").show();
+        $("#my-hero").empty();
+        $("#enemy").empty();
+    } else if (opponentHealth < 0) {
+        $("#matchup").html("<h2>You win!</h2>");
+        remainingEnemy--;
+        if (remainingEnemy > 0) {
+            $("#attackbutton").hide();
+            $("#roundbutton").show();
+        } else {
+            winGame();
+        }
+    }
+}
 
-// console.log(heroObj[fighter].name);
+// initialize/reset the game
+
+function resetGame() {
+    heroChosen = false;
+    opponentChosen = false;
+    roundcount = 0;
+
+    // empty out all information updated through the game
+    $("#pick-hero").empty();
+    $("#my-hero").empty();
+    $("#enemy").empty();
+    $("#actionUpdate1").html("Pick your fighter!");
+    $("#actionUpdate2").html("");
+    $("#matchup").html("");
+    $("#winnerDisplay").hide();
+    $("#loserDisplay").hide();
+
+    // show the hero selection again
+    $("#pick-hero").show();
+
+    // reset the objects containing health and attack scores
+    setObj();
+
+    for (i = 0; i < heroesArray.length; i++) {
+        newImg = $("<img src='assets/images/" + heroesArray[i] + ".jpg'>");
+        newImg.attr("class", "hero");
+        newImg.attr("id", heroesArray[i]);
+        $("#pick-hero").append(newImg);
+    }
+    // set how many enemies are remaining
+    remainingEnemy = 3;
+
+    // hide functionality buttons
+    $("#attackbutton").hide();
+    $("#roundbutton").hide();
+    $("#resetbutton").hide();
+}
+
+// if the round was won, remove the defeated enemy and prompt the user to pick a new enemy
+function nextRound() {
+    heroChosen = true;
+    opponentChosen = false;
+    // remove the defeated enemy;
+    $("#enemy").empty();
+    // hide the button
+    $("#roundbutton").hide();
+    $("#actionUpdate1").html("<h2>Pick your opponent!</h2>");
+    $("#actionUpdate2").html("");
+    $("#matchup").html("");
+    $("#pick-hero").show();
+}
+
+// on total win, show win screen!
+function winGame() {
+    $("#enemy").empty();
+    $("#my-hero").empty();
+    $("#attackbutton").hide();
+    alert("Win!");
+    $("#resetbutton").show();
+    $("#winnerDisplay").show();
+}
+
+////////////////////////////////////////////////////////////////////
+/////////////////////////// GAME CONTROL ///////////////////////////
+////////////////////////////////////////////////////////////////////
+
+// on click event to set the hero and the opponent. For each, grab the ID of the picture clicked
+// then move the img with that ID from the pick-hero section to the "my hero" or "enemy" areas, respectively.
+$("#pick-hero").on("click", ".hero", function(event) {
+    if (!heroChosen) {
+        fighter = $(event.target).attr("id");
+        var gameDiv = $("#" + fighter);
+        gameDiv.appendTo($("#my-hero"));
+        heroChosen = true;
+        $("#actionUpdate1").html("Pick your opponent!");
+    } else if (!opponentChosen) {
+        opponent = $(event.target).attr("id");
+        var gameDiv = $("#" + opponent);
+        gameDiv.appendTo($("#enemy"));
+        opponentChosen = true;
+        $("#attackbutton").show();
+        $("#actionUpdate1").html("");
+        $("#matchup").html("<h2>" + fighter + " vs. " + opponent + "</h2>");
+        // hide all other opponents
+        $("#pick-hero").hide();
+    }
+});
+
+// on click event to ramp up attack power and run the attack function with the inputs for current
+// fighter, current opponent, and current attack power factor
+$("#attackbutton").on("click", function() {
+    roundcount++;
+    attack(heroObj[fighter], heroObj[opponent], roundcount);
+});
+
+// clicking on the reset button should restart the game entirely
+$("#resetbutton").on("click", resetGame);
+
+// clicking on the next round button should start the next round
+$("#roundbutton").on("click", nextRound);
+
+resetGame();
